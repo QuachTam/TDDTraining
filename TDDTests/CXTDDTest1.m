55162fae38503421594f02377df8ab46ae010415
QuachTam <quachngoctam@Quachs-Mac-mini.local>
2013-11-14 13:56:26 +0700

    red

--- TDDTests/CXTDDTest1.m
+++ TDDTests/CXTDDTest1.m
@@ -11,9 +11,13 @@
 
 SPEC_BEGIN(Test1)
     describe(@"TDD kata 1", ^{
-       it(@"Step 1: create Stack, check Stack count equal 0", ^{
-           Stack1 *_stack = [[Stack1 alloc] init];
-           [[theValue([_stack count]) should] equal: theValue(0)];
-       });
+        it(@"Step 1: create Stack, check Stack count equal 0", ^{
+            Stack1 *_stack = [[Stack1 alloc] init];
+            [[theValue([_stack count]) should] equal: theValue(0)];
+        });
+        
+        it(@"Step 2: Stack is empty, push 1 element and check Stack count equal 1", ^{
+            [[theValue([_stack cuont]) should] equal:theValue(1)];
+        });
     });
 SPEC_END
