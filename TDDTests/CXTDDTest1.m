d363b4561d4b5f7f376b79d19a527a9c0271298a
QuachTam <quachngoctam@Quachs-Mac-mini.local>
2013-11-15 14:21:13 +0700

    Step 10
    Step 10

--- TDDTests/CXTDDTest1.m
+++ TDDTests/CXTDDTest1.m
@@ -79,9 +79,7 @@ SPEC_BEGIN(Test1)
             Stack1 *_stack = [[Stack1 alloc] init];
             NSString *_string = [NSString nullMock];
             [_stack push:_string];
-            
             [[[_stack top] should] equal:_string];
-    
             [_stack pop];
         });
     });
