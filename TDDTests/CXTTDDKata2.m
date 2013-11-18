b3b62f40d345252a99017d541330160d150e6962
QuachTam <quachngoctam@Quachs-Mac-mini.local>
2013-11-18 13:52:40 +0700

    start kata 2
    red

---
+++ TDDTests/CXTTDDKata2.m
@@ -0,0 +1,17 @@
+//
+//  CXTTDDKata2.m
+//  TDD
+//
+//  Created by Quach Ngoc Tam on 11/18/13.
+//  Copyright (c) 2013 QsoftVietNam. All rights reserved.
+//
+
+#import "Kiwi.h"
+
+SPEC_BEGIN(kata2)
+    describe(@"Start kata2 with BankAccount", ^{
+       it(@"Step 1 open account with balace = 0", ^{
+           [[theValue([_bank balanceCount]) should] equal:theValue(0)];
+       });
+    });
+SPEC_END
