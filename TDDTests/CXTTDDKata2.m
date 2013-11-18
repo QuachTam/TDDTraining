29aa2d09b3d58e675a1cf750c5a32114f6d39fa6
QuachTam <quachngoctam@Quachs-Mac-mini.local>
2013-11-18 16:08:24 +0700

    green

--- TDDTests/CXTTDDKata2.m
+++ TDDTests/CXTTDDKata2.m
@@ -12,7 +12,10 @@ SPEC_BEGIN(kata2)
     describe(@"Start kata2 with BankAccount", ^{
        it(@"Step 1 open account with balace = 0", ^{
            BankAccount *_bank = [[BankAccount alloc] init];
+           
            [[theValue([_bank balanceCount]) should] equal:theValue(0)];
        });
+        
+        
     });
 SPEC_END
